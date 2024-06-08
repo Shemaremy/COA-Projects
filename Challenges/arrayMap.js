0ba1d4844f650e0e92cd6fd11a1b3c223ddecf54
Shemaremy <remyshema20@gmail.com>
2024-06-08 17:20:06 +0300

    The Last Commit

---
+++ Challenges/arrayMap.js
@@ -0,0 +1,94 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let Array = [];
+let Target;
+let sum = 0;
+
+
+
+
+
+//This function checks if the sub array exist or doesnt exist and returns the final output
+function Checker () {
+
+    let found = false;
+    
+    for (let p = 0; p < Array.length; p++) {
+        let Sum = 0;
+        for (let i = p; i < Array.length; i++) {
+            Sum += Array[i];
+            if (Sum === Target) {
+                found = true;
+                break;
+            }
+            if (Sum > Target) {
+                break;
+            }
+        }
+        if (found) {
+            break;
+        }
+    }
+
+    console.log(found);
+
+}
+
+
+
+// Checks if the array contains elements between 1 and 100,000
+function lengthCheck(){
+
+    const min = 1;
+    const max = 100000;
+
+
+
+    if (Array.length > min && Array.length < max) {
+        Checker();
+    } else {
+        console.log("Please, enter elements in array which are between 1 and 100,000")
+    }    
+}
+
+
+
+// This one asks to enter the array elements one by one
+function collectInput(length, currentIndex) {
+    if (currentIndex > length) {
+        lengthCheck();
+        rl.close();
+        return;
+    }
+
+    rl.question(`Enter element ${currentIndex}: `, (input) => {
+        Array.push(parseInt(input)); // Add the input to the array after converting it to a number
+        sum += parseInt(input); // Add the input to the sum
+        collectInput(length, currentIndex + 1);
+    });
+}
+
+
+// Basic questions. Asks Length of array and its Target sum
+function startInputCollection() {
+    rl.question('Enter length of array: ', (length) => {
+        rl.question('Now enter the Target: ', (inputTarget) => {
+            Target = parseInt(inputTarget);   // Convert it to an integer
+            collectInput(parseInt(length), 1);
+        });
+    });
+}
+
+
+
+
+
+
+startInputCollection();
+
+
+
---
+++ Challenges/stringTransform.js
@@ -0,0 +1,65 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+
+// If divisible by 3, reverse the string
+function three(Name) {
+    let rev = Name.split('').reverse().join('');
+    console.log(rev);
+}
+
+
+// If divisible by 5, change to ASCII
+function Five(Name) {
+    let ascii = [];
+
+    for (let i = 0; i < Name.length; i++) {
+        ascii.push(Name.charCodeAt(i));
+    }
+
+    console.log(ascii);
+
+}
+
+
+// If divisible by 3 and 5, perform all operations
+function FiveandThree(Name) {
+    let rev = Name.split('').reverse().join('');
+    console.log("Reversed : " +rev);
+
+    let ascii = [];
+
+    for (let i = 0; i < Name.length; i++) {
+        ascii.push(Name.charCodeAt(i));
+    }
+
+    console.log("ASCII Codes : " +ascii);
+
+}
+
+
+// Function to check first condition (Length of string)
+function checkLength(Name) {
+    if (len % 3 === 0 && len % 5 === 0) {
+        FiveandThree(Name);
+    } else if (len % 3 === 0) {
+        three(Name);
+    } else if (len % 5 === 0) {
+        Five(Name);
+    } else {
+        console.log("The length of the string is neither divisible by 5 nor 3");
+    }
+    
+}
+
+
+
+rl.question('Enter your name : ', (Name) => {
+    len = Name.length
+    checkLength(Name);
+    rl.close();
+});
