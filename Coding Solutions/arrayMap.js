a03a55a5c5ec756ce0ee2ad294e7102afbde4c24
Remy <remyshema20@gmail.com>
2024-06-15 20:52:03 +0200

    Update arrayMap.js

--- Coding Solutions/arrayMap.js
+++ Coding Solutions/arrayMap.js
@@ -76,9 +76,10 @@ function collectInput(length, currentIndex) {
 // Basic questions. Asks Length of array and its Target sum
 function startInputCollection() {
     rl.question('Enter length of array: ', (length) => {
+        let len = parseInt(length);
         rl.question('Now enter the Target: ', (inputTarget) => {
             Target = parseInt(inputTarget);   // Convert it to an integer
-            collectInput(parseInt(length), 1);
+            collectInput(parseInt(len, 1);
         });
     });
 }
